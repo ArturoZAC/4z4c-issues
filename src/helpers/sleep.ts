1a7c79f2627057a8115fd5c5bf201e340c0aa308
Arturo <arturoyz2105@gmail.com>
2025-03-24 21:43:11 -0500

    issues-4z4c

---
+++ src/helpers/sleep.ts
@@ -0,0 +1,13 @@
+export const sleep = (miliseconds: number) => {
+
+  return new Promise((resolve, reject) =>  {
+
+    if( miliseconds < 0 ){
+      reject('Miliseconds must be greather than zero.');
+      return;
+    }
+
+    setTimeout(() => { resolve(true) }, miliseconds);
+  } )
+
+};
