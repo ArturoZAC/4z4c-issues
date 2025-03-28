01228ddbcba54e176c2bacfd4e00b8f1868002e9
Arturo <arturoyz2105@gmail.com>
2025-03-28 11:42:37 -0500

    finalissues

--- src/issues/views/ListViewInfinite.tsx
+++ src/issues/views/ListViewInfinite.tsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
-import { useIssues } from '../../hooks';
+import { useIssuesInfinite } from '../../hooks';
 import { LoadingSpinner } from '../../shared';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
 import { State } from '../../interfaces';
 
-export const ListView = () => {
+export const ListViewInfinite = () => {
 
   const [state, setState] = useState<State>(State.All);
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
 
-  const { issuesQuery, nextPage, page, prevPage } = useIssues({
+  const { issuesQuery } = useIssuesInfinite({
     state: state,
     selectedLabels: selectedLabels
   });
 
-  const issues = issuesQuery.data ?? [];
+  const issues = issuesQuery.data?.pages.flat() ?? [];
 
   const onLabelSelected = (label: string) => {
     if (selectedLabels.includes(label)) {
@@ -32,27 +32,20 @@ export const ListView = () => {
           issuesQuery.isLoading
             ? <LoadingSpinner />
             :
-            <>
+            <div className='flex flex-col justify-center'>
               <IssueList issues={issues} onStateChange={setState} state={state} />
-
-              <div className='flex justify-between items-center'>
-                <button
-                  onClick={prevPage}
-                  className='p-2 bg-blue-500 rounded-md hover:bg-blue-700 transition-all'
-                >
-                  Anteriores
-                </button>
-
-                <span>{page}</span>
-
-                <button
-                  onClick={nextPage}
-                  className='p-2 bg-blue-500 rounded-md hover:bg-blue-700 transition-all'
-                >
-                  Siguientes
-                </button>
-              </div>
-            </>
+              <button
+                onClick={() => issuesQuery.fetchNextPage()}
+                disabled={issuesQuery.isFetchingNextPage}
+                className='p-2 bg-blue-500 rounded-md hover:bg-blue-700 transition-all disabled:bg-gray-500'
+              >
+                {
+                  issuesQuery.isFetchingNextPage
+                    ? 'Cargando...'
+                    : 'Cargar más'
+                }
+              </button>
+            </div>
         }
       </div>
       <div className="col-span-1 px-2">
